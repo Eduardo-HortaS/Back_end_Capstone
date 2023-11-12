72d62f1082021d24896d68a3efb2269de2d5f423
Eduardo-HortaS <68471383+Eduardo-HortaS@users.noreply.github.com>
2023-11-12 13:19:10 -0300

    Setup models

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
-# Register your models here.
+# Define a list of models to register
+models = [Menu, Booking]
+
+# Register all models in the list using a loop
+for model in models:
+    admin.site.register(model)
